8dbeb41e8bec6b03da1284fd19173330f980bf8b
mac air 2020 <macair2020@macs-MacBook-Air.local>
2025-04-09 19:44:28 -0400

    first commit

---
+++ comment.py
@@ -0,0 +1,30 @@
+#A one liner comment: is a comment that is written in a single line
+print("Hello World") # This code prints "Hello World" to the console
+# A multi-line comment: is a comment that is written in multiple lines to explainm a complex code or logic
+# it is representrdf by triple quotes (""") or (''')
+"""A multi-line comment: is a comment that is written in multiple lines to explainm a complex code or logic"""
+print() #this is a function that displays text or values on the screen.
+
+name = "Alex" 
+name2 ='John'
+age = 17 
+print(name, " and ", name2, 'are both', age, " years old")
+#printing line 11 using f string
+print(f"{name} and {name2} are both {age} years old")
+
+name3 = "Sam"
+hometown = "Blantyre"
+print("My name is", name3, "and i am from", hometown)
+
+#Printing math
+print (3+4)
+print("3+4=", 3+4 )
+
+#Data type: There are 4 date types in python
+#1. Int stands for integer: interger is a whole number
+#2. float: float is a decimal number
+#3. Str: stands for string
+#4. bool stans for boolean: boolean is any statement that will either be true or false
+
+
+      
